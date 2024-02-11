2a1ad66a692207490dd2a6dded6b82f851999aa9
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-02-11 15:58:37 +0400

    Update method2.java

--- method2.java
+++ method2.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
-public class method2{
-	public static void main(String[] arg){
-		Scanner sc=new Scanner(System.in);
-		for (int x=1; x>0; x++){
-			String a=sc.nextLine();
-			if (a.equals("x")){
-				System.out.println("We come out from loop");
-				break;
-			}
-			System.out.println(a +"*");
-				
-		}
-	}
+public class method2 {
+  public static void main(String[] arg) {
+    Scanner sc = new Scanner(System.in);
+    for (int x = 1; x > 0; x++) {
+      String a = sc.nextLine();
+      if (a.equals("x")) {
+        System.out.println("We come out from loop");
+        break;
+      }
+      System.out.println(a + "*");
+
+    }
+  }
 }
-			
